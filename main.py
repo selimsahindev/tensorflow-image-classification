1fc39f02d5c0e441e7ab6ffd10397dca20f80f6d
Selim Şahin <selimsahin.sns@gmail.com>
2021-05-14 10:58:49 +0300

    training, using and evaluating the model

--- main.py
+++ main.py
@@ -50,18 +50,105 @@ for i in range(25):
     plt.grid(False)
     plt.imshow(train_images[i], cmap=plt.cm.binary)
     plt.xlabel(class_names[train_labels[i]])
-
 plt.show()
 
+# Building the model
+# Setting up layers:
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10)
+])
 
+# Compile the model
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
 
+# Training the model
+# Feed the model:
+model.fit(train_images, train_labels, epochs=10)
 
+# Evaluating accuracy
+test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
 
+print('\nTest accuracy:', test_acc)
 
+# Make predictions
+probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
+predictions = probability_model.predict(test_images)
 
+# Here, the model has predicted the label for each image in the testing set. Let's take a look at the first prediction:
+print('Predictions array: ', predictions[0])
 
+# A prediction is an array of 10 numbers.
+# They represent the model's "confidence" that the image corresponds to each of the 10 different articles of clothing.
+# We can see which label has the highest confidence value:
+print('Highest value: ', np.argmax(predictions[0]))
 
+# According to the output, the model is most confident that this image is an ankle boot, or class_names[9].
+# Examining the test label shows that this classification is correct:
+print(test_labels[0])
 
+# Graph this to look at the full set of 10 class predictions.
+def plot_image(i, predictions_array, true_label, img):
+    true_label, img = true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
 
+    plt.imshow(img, cmap=plt.cm.binary)
 
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
 
+    plt.xlabel("{} {:2.0f}% ({})".format(
+        class_names[predicted_label],
+        100*np.max(predictions_array),
+        class_names[true_label]
+    ), color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+    true_label = true_label[i]
+    plt.grid(False)
+    plt.xticks(range(10))
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+
+# Verify Predictions
+# Color correct predictions in blue and incorrect predictions in red.
+num_rows = 5
+num_cols = 3
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+    plt.subplot(num_rows, 2*num_cols, 2*i+1)
+    plot_image(i, predictions[i], test_labels, test_images)
+    plt.subplot(num_rows, 2*num_cols, 2*i+2)
+    plot_value_array(i, predictions[i], test_labels)
+plt.tight_layout()
+plt.show()
+
+# Using the trained model
+img = test_images[1]
+
+# tf.keras models are optimized to make predictions on a batch, or collection, of examples at once.
+# Accordingly, even though we're using a single image, we need to add it to a list:
+img = (np.expand_dims(img,0))
+
+# Now predict the correct label for this image:
+predictions_single = probability_model.predict(img)
+plot_value_array(1, predictions_single[0], test_labels)
+_ = plt.xticks(range(10), class_names, rotation=45)
+
+# tf.keras.Model.predict returns a list of lists—one list for each image in the batch of data.
+# Grab the predictions for our (only) image in the batch:
+np.argmax(predictions_single[0])
